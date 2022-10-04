e25ef33b329986057fc41a06e4bd7bbe370eb910
Jimmy Hogoboom <jimmy@differential.com>
2022-10-04 16:10:29 -0700

    Implement space brake (and broken background)

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 
+const BRAKE_VELOCITY_DELTA = 15;
 
 export default class MainGame {
   constructor() {
@@ -11,6 +12,7 @@ export default class MainGame {
     this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
+    this.sky = this.add.tileSprite(1280, 720, 800, 600, 'sky');
   }
 
   create() {
@@ -28,6 +30,7 @@ export default class MainGame {
       this.keys = this.input.keyboard.addKeys({
         'lateralLeft': 188,
         'lateralRight': 190,
+        'spaceBrake': Phaser.Input.Keyboard.KeyCodes.X,
       })
     }
 
@@ -40,7 +43,7 @@ export default class MainGame {
       zoomIn: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
       zoomOut: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
       acceleration: 0.06,
-      // drag: 0.0005,
+      drag: 0.0005,
       maxSpeed: 1.0
     };
 
@@ -60,7 +63,7 @@ export default class MainGame {
     if (this.cursors.up.isDown) {
       this.physics.velocityFromRotation(this.ship.rotation, 600, this.ship.body.acceleration);
     } else if (this.cursors.down.isDown) {
-      this.physics.velocityFromRotation(this.ship.rotation, -600, this.ship.body.acceleration);
+      this.physics.velocityFromRotation(this.ship.rotation, -400, this.ship.body.acceleration);
     }
     else {
       this.ship.setAcceleration(0);
@@ -68,11 +71,46 @@ export default class MainGame {
 
     if (this.keys.lateralLeft.isDown) {
       const newRads = this.addDegreesToRads(this.ship.rotation, -90);
-      this.physics.velocityFromRotation(newRads, 600, this.ship.body.acceleration);
+      this.physics.velocityFromRotation(newRads, 400, this.ship.body.acceleration);
     } else if (this.keys.lateralRight.isDown) {
       const newRads = this.addDegreesToRads(this.ship.rotation, 90);
-      this.physics.velocityFromRotation(newRads, 600, this.ship.body.acceleration);
+      this.physics.velocityFromRotation(newRads, 400, this.ship.body.acceleration);
     }
+
+    if (this.keys.spaceBrake.isDown) {
+      const [x, y] = [this.ship.body.velocity.x, this.ship.body.velocity.y];
+
+      if (x != 0) {
+        const velocityDelta =
+          Math.abs(this.ship.body.velocity.x) < BRAKE_VELOCITY_DELTA
+            ? Math.abs(this.ship.body.velocity.x)
+            : BRAKE_VELOCITY_DELTA;
+
+        if (x > 0) {
+          this.ship.body.velocity.x -= velocityDelta;
+        } else {
+          this.ship.body.velocity.x += velocityDelta;
+        }
+      }
+
+      if (y != 0) {
+        const velocityDelta =
+          Math.abs(this.ship.body.velocity.y) < BRAKE_VELOCITY_DELTA
+            ? Math.abs(this.ship.body.velocity.y)
+            : BRAKE_VELOCITY_DELTA;
+
+        if (y > 0) {
+          this.ship.body.velocity.y -= velocityDelta;
+        } else {
+          this.ship.body.velocity.y += velocityDelta;
+        }
+      }
+
+      console.log({ x, y, ship: this.ship.body.velocity })
+    }
+
+    this.sky.tilePositionX += this.ship.body.deltaX();
+    this.sky.tilePositionY += this.ship.body.deltaY();
   }
 
   addDegreesToRads(rads, degrees) {
--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import MainGame from './Game.js';
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1280,
+  height: 720,
   physics: {
     default: 'arcade',
     arcade: {
