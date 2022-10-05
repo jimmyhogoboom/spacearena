3d8cfd625ee7078f3fe8c8bd67aad608275a1dd6
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2022-10-05 19:34:15 -0400

    Update config with browser window dimensions

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import MainGame from './Game.js';
 
 const config = {
   type: Phaser.AUTO,
-  width: 1280,
-  height: 720,
+  width: window.innerWidth,
+  height: window.innerHeight,
   physics: {
     default: 'arcade',
     arcade: {
